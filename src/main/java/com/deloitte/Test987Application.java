5c26fb88e481d26061b149d7238aa55639e55392
shaikhaamer <89386754+shaikhaamer@users.noreply.github.com>
2021-10-12 19:47:04 +0530

    Initial Commit

---
+++ src/main/java/com/deloitte/Test987Application.java
@@ -0,0 +1,13 @@
+package com.deloitte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test987Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test987Application.class, args);
+	}
+
+}
